027baf1694a0bd2df98c037149593adc0557fedb
andres990420 <72096387+andres990420@users.noreply.github.com>
2025-06-17 20:16:58 -0400

    Refactor budgets interface and component: improve formatting of categories type definition; enhance fetch logic with error handling; streamline function definitions for better readability.

--- frontend/src/pages/Budgets.tsx
+++ frontend/src/pages/Budgets.tsx
@@ -11,31 +11,43 @@ interface IBudgets {
   name: string;
   currentAmount: number;
   maxAmount: number;
-  categories: [{ type: string; color: string; description: string , currentAmount: number, maxAmount: number}];
+  categories: [
+    {
+      type: string;
+      color: string;
+      description: string;
+      currentAmount: number;
+      maxAmount: number;
+    }
+  ];
   createdAt: Date;
   updtedAt: Date;
 }
 
 export default function Budgets() {
   const [isModalActive, setIsModalActive] = useState(false);
-  
+
   const [budgets, setBudgets] = useState(Array<IBudgets>);
-  useEffect( () => {
-    async function fetchApiBudgets(){
-      const response = await fetch("http://localhost:4000/budgets");
-      const data = await response.json()
-      setBudgets(data)
-    } 
-    fetchApiBudgets()
+  useEffect(() => {
+    async function fetchApiBudgets() {
+      try {
+        const response = await fetch("http://localhost:4000/budgets");
+        const data = await response.json();
+        setBudgets(data);
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchApiBudgets();
   }, []);
   let percentage;
-  
-  function cancelForm(){
+
+  function cancelForm() {
     setIsModalActive(!isModalActive);
   }
 
-  function newBudget(){
-    setIsModalActive(true)
+  function newBudget() {
+    setIsModalActive(true);
   }
   return (
     <main className="m-3">
@@ -49,32 +61,37 @@ export default function Budgets() {
           </Button>
         </div>
         <div className="items-center">
-          {budgets.map((budget) => (
-          percentage = budget.currentAmount / budget.maxAmount,
-          <BudgetsTable 
-            key={budget.id}
-            budgetName={budget.name}
-            limitValue={budget.maxAmount}
-            currentValue={budget.currentAmount}
-            percentage={percentage}
-            // childrensTables={budget.categories}
-        />
-        ))}
-        <BudgetsTable
-          limitValue={20000}
-          currentValue={10000}
-          percentage={50}
-          budgetName="Nombre del presupuesto"
-        />
+          {budgets.map(
+            (budget) => (
+              (percentage = budget.currentAmount / budget.maxAmount),
+              (
+                <BudgetsTable
+                  key={budget.id}
+                  budgetName={budget.name}
+                  limitValue={budget.maxAmount}
+                  currentValue={budget.currentAmount}
+                  percentage={percentage}
+                  // childrensTables={budget.categories}
+                />
+              )
+            )
+          )}
+          <BudgetsTable
+            limitValue={20000}
+            currentValue={10000}
+            percentage={50}
+            budgetName="Nombre del presupuesto"
+          />
         </div>
-        
       </section>
-      <Modal isActive={isModalActive} setIsActive={setIsModalActive} title="Nuevo Presupuesto">
-        <BudgetForm cancelForm={cancelForm}/>
+      <Modal
+        isActive={isModalActive}
+        setIsActive={setIsModalActive}
+        title="Nuevo Presupuesto"
+      >
+        <BudgetForm cancelForm={cancelForm} />
       </Modal>
-      <section>
-        
-      </section>
+      <section></section>
     </main>
   );
 }
